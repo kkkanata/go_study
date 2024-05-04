f3d2eb28fbd7b7a08b96cbb7d6275d1b78a8c042
User name <nanasimui@icloud.com>
2024-05-04 20:13:24 +0900

    study

--- at10.go
+++ at10.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fmt"
-)
-
+) // 自作コード(不完全)
 func main() {
-	var n int
-	fmt.Scan(&n)
-	a := make([]int, n)
-	b := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&a[i])
-		fmt.Scan(&b[i])
+	var s string
+	fmt.Scan(&s)
+	var ss []string = []string{"dream", "dreamer", "erase", "eraser"}
+	max := len(s)
+OuterLoop:
+	for max > 0 {
+		for i := 0; i < len(ss) && max != 0; i++ {
+			less := max - len(ss[i])
+			if less >= 0 {
+				if s[less:max] == ss[i] {
+					max = max - len(ss[i])
+					i = 0
+				} else if i == len(ss)-1 {
+					break OuterLoop
+				}
+			}
+		}
 	}
-	sum := 0
-	sum = b[0] - a[0] + 1
-	max := b[0]
-	less := a[0]
-	for i := 1; i < n; i++ {
-		diff1 := max - a[i]
-		if max <= 
+	if max == 0 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
 	}
-	fmt.Printf("%d", sum)
-
 }
