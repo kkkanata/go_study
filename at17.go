539270fb72f91e238665827cc4106eac9af2cb93
User name <nanasimui@icloud.com>
2024-05-05 19:54:49 +0900

    study

---
+++ at17.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var patterns []string = []string{"dream", "dreamer", "erase", "eraser"}
+	var s string
+	fmt.Scan(&s)
+	var can bool
+	for len(s) > 0 {
+		can = false
+		for _, value := range patterns {
+			if strings.HasSuffix(s, value) {
+				can = true
+				s = s[:len(s)-len(value)]
+				break
+			}
+		}
+		if !can {
+			fmt.Println("No")
+			return
+		}
+	}
+	fmt.Println("Yes")
+}
