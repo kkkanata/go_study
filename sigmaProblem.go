66878f6597aca2c703e8d0e97397cd28d426d528
User name <nanasimui@icloud.com>
2024-05-13 23:37:02 +0900

    shakutori

--- sigmaProblem.go
+++ sigmaProblem.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
 	var n int
 	fmt.Scan(&n)
-	a:=make([]int, n)
+	a := make([]int, n)
 	sum := 0
-	for i:=0;i<n;i++{
+	for i := 0; i < n; i++ {
 		fmt.Scan(&a[i])
-		sum+=a[i]
+		sum += a[i]
 	}
-	sum = sum/(n-1)
+	sum *= n - 1
+	sort.Ints(a)
+	r := n
+	cnt := 0
+	for i := 0; i < n; i++ {
+		r = max(r, i+1)
+		for r-1 > i && a[r-1]+a[i] >= 100000000 {
+			r--
+		}
+		cnt += n - r
+	}
+	res := sum - (100000000 * cnt)
+	fmt.Println(res)
 
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
