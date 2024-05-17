62168af461510dd5c2bebee8a18c2c9bbecab4ad
User name <nanasimui@icloud.com>
2024-05-17 18:47:11 +0900

    study

--- atManyReplacement.go
+++ atManyReplacement.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	
 	"fmt"
 )
+
 //bufio, io, strconv 等を調べるとよい
 
 func main() {
@@ -13,24 +13,25 @@ func main() {
 	fmt.Scan(&s)
 	fmt.Scan(&q)
 	sc := make(map[string][]int, n)
-	for i:=0;i<n;i++{
-		sc[string(s[i])] = append(sc[string(s[i])], i) 
+	for i := 0; i < n; i++ {
+		sc[string(s[i])] = append(sc[string(s[i])], i)
 	}
 	result := make([]string, n)
-	for i:=0;i<n;i++{
+	for i := 0; i < n; i++ {
 		result[i] = string(s[i])
 	}
 	var c, d string
-	for i:=0;i<q;i++{
+	for i := 0; i < q; i++ {
 		fmt.Scan(&c)
 		fmt.Scan(&d)
-		if _, ok := sc[c]; ok {
-			for j:=0;j<len(sc[c]);j++{
+		if _, ok := sc[c]; ok && c != d {
+			for j := 0; j < len(sc[c]); j++ {
 				result[sc[c][j]] = d
 				//mapの更新処理
-				sc[d] = append(sc[d], j)
+				sc[d] = append(sc[d], sc[c][j])
 			}
-			delete(sc,c)
+			fmt.Println(result)
+			delete(sc, c)
 		}
 	}
 	for _, v := range result {
