82c1cb8a66fcefc644898c9e1a495f202472d255
kkkanata <nanasimui@icloud.com>
2024-05-17 12:21:03 +0900

    study

--- atManyReplacement.go
+++ atManyReplacement.go
@@ -12,6 +12,10 @@ func main() {
 	var s string
 	fmt.Scan(&s)
 	fmt.Scan(&q)
+	sc := make(map[string][]int, n)
+	for i:=0;i<n;i++{
+		sc[string(s[i])] = append(sc[string(s[i])], i) 
+	}
 	result := make([]string, n)
 	for i:=0;i<n;i++{
 		result[i] = string(s[i])
@@ -20,9 +24,12 @@ func main() {
 	for i:=0;i<q;i++{
 		fmt.Scan(&c)
 		fmt.Scan(&d)
-		for j:=0; j<n;j++{
-			if string(result[j]) == c {	
-			result[j] = d
+		if _, ok := sc[c]; ok {
+			for j:=0;j<len(sc[c]);j++{
+				result[sc[c][j]] = d
+				//mapの更新処理
+				delete(sc,c)
+				
 			}
 		}
 	}
