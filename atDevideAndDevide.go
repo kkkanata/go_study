c9c8fbc8d8b7183cc725a2a1ec133bf063a2c15e
User name <nanasimui@icloud.com>
2024-05-20 22:45:12 +0900

    study

--- atDevideAndDevide.go
+++ atDevideAndDevide.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	
 	"fmt"
-	"strconv"
 )
+
+func recursion(n int) int {
+	if n == 1 {
+		return 0
+	} else {
+		return recursion(n/2) + recursion((n+1)/2) + n
+	}
+}
 
 func main() {
 	var n int
 	fmt.Scan(&n)
-	
+	result := recursion(n)
+	fmt.Println(result)
 }
---
+++ work.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	i := 123.76
+	x := math.Ceil(i)
+	fmt.Println(x)
+}
