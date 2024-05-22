c2fa0a24afb73b56896d264c6303237ab5ec4fcf
User name <nanasimui@icloud.com>
2024-05-22 23:26:52 +0900

    study

---
+++ at40.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*	var s string
+		fmt.Scan(&s)
+		var t string
+		fmt.Scan(&t)
+	*/
+	var s string
+	fmt.Scan(&s)
+	fmt.Println(s[0], s[1])
+}
