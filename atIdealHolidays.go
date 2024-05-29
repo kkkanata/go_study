1631d7886c8ff67779f7b80891f2e2d22e4e785c
User name <nanasimui@icloud.com>
2024-05-29 22:43:58 +0900

    study

--- atIdealHolidays.go
+++ atIdealHolidays.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	var N int
-	fmt.Scan(&N)
-	S := make([]bool, N)
-	var w int
-	for i := 0; i < N; i++ {
-		fmt.Scan(&w)
-		if w == 1 {
-			S[i] = true
-		}
-	}
-	var l, r int
-	for i := 0; i < N; i++ {
-		if S[i] {
-
-		}
-	}
 
 }
