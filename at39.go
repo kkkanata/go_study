1ffc0446d655c9fe2c4435d92600b0a781219e47
kkkanata <nanasimui@icloud.com>
2024-05-31 09:36:19 +0900

    study

--- at39.go
+++ at39.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	var n int
 	fmt.Scan(&n)
-	a := make([]int, n)
+	var a int
 	count := 0
 	for i:=0;i<n;i++{
-		fmt.Scan(&a[i])
-		count+=a[i]
+		fmt.Scan(&a)
+		count+=a
 		if count < 0 {
 			count = 0
 		}
