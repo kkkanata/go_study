72c9d8b492dab65e8a32a88520fad824a8b1e2dd
kkkanata <nanasimui@icloud.com>
2024-06-04 12:41:25 +0900

    study

--- interfaceStudy.go
+++ interfaceStudy.go
@@ -7,7 +7,25 @@ type Blog struct {
 	content string
   }
 
-func GetFullArticle(b Blog) string {
+/*func GetFullArticle(b Blog) string {
+	return b.title + "\n" + "------------" + "\n" + b.content
+  }
+  */
+  //ポインタレシーバでないため値の変更ができない
+  func (b Blog) ValueChange2(s string) {
+	b.title += s
+	b.content += s
+	return
+  }
+
+  //ポインタレシーバであるため値の変更が可能
+  func (b *Blog) ValueChange(s string) {
+	b.title += s
+	b.content += s
+	return
+  }
+
+  func (b Blog) GetFullArticle() string {
 	return b.title + "\n" + "------------" + "\n" + b.content
   }
 
@@ -15,6 +33,7 @@ func main() {
 	var b Blog
 	b.title = "title!"
 	b.content = "content!"
-	s := GetFullArticle(b)
-	fmt.Println(s)
+	s := "happy!"
+	b.ValueChange(s)
+	fmt.Println(b.GetFullArticle())
 }
